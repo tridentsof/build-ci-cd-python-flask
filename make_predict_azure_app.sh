8432c1adfe8779cc79f99e0b1a6d1d18f8b6112d
Tri Dang <51437905+tridentsof@users.noreply.github.com>
2023-11-12 12:08:23 +0700

    update python code to run

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
+     -X POST https://trident-webapp-webapp-linux-predict-trident.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
